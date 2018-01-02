033a613eccf8ae02761bf42c0670d963920c2899
Ivan Rojo <ivanrojo07@gmail.com>
2018-01-02 12:03:12 -0600

    12:03

--- resources/views/clientes/index.blade.php
+++ resources/views/clientes/index.blade.php
@@ -28,13 +28,8 @@
 				</tr>
 			</thead>
 			@foreach($personals as $personal)
-<<<<<<< HEAD
-				<tr class="active">
-					<td>{{$personal->id}}</td>
-=======
 				<tr class="active"{{--  onclick="vistarapida({{$personal->id}})" --}} href="#{{$personal->id}}">
 					<td><a rel="#{{$personal->id}}"> {{$personal->id}}</a></td>
->>>>>>> 3959c603c50cefa9e10ac7950f3b15e83ae0fa03
 					<td>
 						@if ($personal->tipopersona == "Fisica")
 						{{$personal->nombre}} {{ $personal->apellidopaterno }} {{ $personal->apellidomaterno }}
@@ -47,11 +42,7 @@
 					<td>{{ strtoupper($personal->rfc) }}</td>
 					<td>{{$personal->vendedor}}</td>
 					<td>
-<<<<<<< HEAD
-							<a class="btn btn-success btn-sm"{{-- href="{{ route('clientes.show',['cliente'=>$personal]) }}" --}}><i class="fa fa-eye" aria-hidden="true"></i> Ver</a>
-=======
 							<a class="btn btn-success btn-sm" href="{{ route('clientes.show',['cliente'=>$personal]) }}"><i class="fa fa-eye" aria-hidden="true"></i> Ver</a>
->>>>>>> 3959c603c50cefa9e10ac7950f3b15e83ae0fa03
 							<a class="btn btn-info btn-sm" href="{{ route('clientes.edit',['cliente'=>$personal]) }}"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Editar</a>
 				</tr>
 				</td>
@@ -64,66 +55,6 @@
 </div>
 @foreach ($personals as $personal)
 	{{-- expr --}}
-<<<<<<< HEAD
-	<div class="container" id="tab">
-				<div role="application" class="panel panel-group" >
-					<div class="panel-default">
-						<div class="panel-heading"><h4>Datos del cliente:</h4></div>
-						<div class="panel-body">
-							<div class="col-md-12 offset-md-2 mt-3">
-			  					<div class="form-group col-lg-3 col-md-3 col-sm-6 col-xs-12">
-			    					<label class="control-label" for="tipopersona">Tipo de Persona:</label>
-			    					<dd>{{ $personal->tipopersona }}</dd>
-			  					</div>
-			  					<div class="form-group col-lg-3 col-md-3 col-sm-6 col-xs-12">
-			  						<label class="control-label" for="alias">Alias:</label>
-			  						<dd>{{ $personal->alias }}</dd>
-			  					</div>
-			  					<div class="form-group col-lg-3 col-md-3 col-sm-6 col-xs-12">
-			  						<label class="control-label" for="rfc">RFC:</label>
-			  						<dd>{{ $personal->rfc }}</dd>
-			  					</div>
-			  					<div class="form-group col-lg-3 col-md-3 col-sm-6 col-xs-12">
-			  						<label class="control-label" for="vendedor">Vendedor:</label>
-			  						<dd>{{ $personal->vendedor }}</dd>
-			  					</div>
-							</div>
-						@if ($personal->tipopersona == "Fisica")
-								{{-- true expr --}}
-							<div class="col-md-12 offset-md-2 mt-3" id="perfisica">
-								<div class="form-group col-lg-3 col-md-3 col-sm-6 col-xs-12">
-			  						<label class="control-label" for="nombre">Nombre(s):</label>
-			  						<dd>{{ $personal->nombre }}</dd>
-			  					</div>
-			  					<div class="form-group col-lg-3 col-md-3 col-sm-6 col-xs-12">
-			  						<label class="control-label" for="apellidopaterno">Apellido Paterno:</label>
-			  						<dd>{{ $personal->apellidopaterno }}</dd>
-			  					</div>
-			  					<div class="form-group col-lg-3 col-md-3 col-sm-6 col-xs-12">
-			  						<label class="control-label" for="apellidomaterno">Apellido Materno:</label>
-			  						<dd>{{ $personal->apellidomaterno }}</dd>
-			  					</div>
-							</div>
-						@else
-								{{-- false expr --}}
-							<div class="col-md-12 offset-md-2 mt-3" id="permoral">
-								<div class="form-group col-lg-3 col-md-3 col-sm-6 col-xs-12">
-
-			  						<label class="control-label" for="razonsocial">Razon Social:</label>
-			  						<dd>{{ $personal->razonsocial }}</dd>
-			  					</div>
-							</div>
-						@endif
-						</div>
-					</div>
-					<ul role="tablist" class="nav nav-tabs nav-pills nav-justified">
-						<li role="tab" tabindex="0" aria-controls="tabs-1" aria-labelledby="ui-id-1" aria-selected="true" aria-expanded="true" class="ui-tabs-tab ui-corner-top ui-state-default ui-tab ui-tabs-active ui-state-active active"><a href="#tab1" tabindex="-1">Dirección Fiscal:</a></li>
-						<li role="tab" tabindex="-1" class="ui-tabs-tab ui-corner-top ui-state-default ui-tab" aria-controls="tabs-2" aria-labelledby="ui-id-2" aria-selected="false" aria-expanded="false"><a href="#tab2" role="tab" tabindex="-1" class="ui-tabs-anchor" id="ui-id-2">Dirección Fisica:</a></li>
-						<li role="tab" tabindex="-1" class="ui-tabs-tab ui-corner-top ui-state-default ui-tab" aria-controls="tabs-3" aria-labelledby="ui-id-3" aria-selected="false" aria-expanded="false"><a href="#tab3" role="tab" tabindex="-1" class="ui-tabs-anchor" id="ui-id-3">Contacto:</a></li>
-						<li role="tab" tabindex="-1" class="ui-tabs-tab ui-corner-top ui-state-default ui-tab" aria-controls="tabs-3" aria-labelledby="ui-id-3" aria-selected="false" aria-expanded="false"><a href="#tab4" role="tab" tabindex="-1" class="ui-tabs-anchor" id="ui-id-3">Datos Generales:</a></li>
-					</ul>
-					<div class="panel-default pestana" aria-hidden="false" id="tab1" style="display: block;">
-=======
 	<div class="persona" id="{{$personal->id}}">
 	<div class="container" id="tab">
 				<div role="application" class="panel panel-group" >
@@ -144,7 +75,6 @@
 						<li role="tab" tabindex="-1" class="ui-tabs-tab ui-corner-top ui-state-default ui-tab" aria-controls="tabs-3" aria-labelledby="ui-id-3" aria-selected="false" aria-expanded="false"><a href="#tab4{{$personal->id}}" role="tab" tabindex="-1" class="ui-tabs-anchor" id="ui-id-3">Datos Generales:</a></li>
 					</ul>
 					<div class="panel-default pestana" aria-hidden="false" id="tab1{{$personal->id}}" style="display: block;">
->>>>>>> 3959c603c50cefa9e10ac7950f3b15e83ae0fa03
 						<div class="panel-heading">Dirección Fiscal:</div>
 						<div class="panel-body">
 							<div class="col-md-12 offset-md-2 mt-3">
@@ -199,12 +129,7 @@
 							</div>
 						</div>
 					</div>
-<<<<<<< HEAD
-					<div class="panel-default pestana" id="tab2">
-=======
 					<div class="panel-default pestana" id="tab2{{$personal->id}}">
->>>>>>> 3959c603c50cefa9e10ac7950f3b15e83ae0fa03
-
 						<div class="panel-heading">Dirección Fisica:</div>
 						<div class="panel-body">
 							@if (count($personal->direccionFisica) == 0 )
@@ -262,11 +187,9 @@
 							@endif
 						</div>
 					</div>
-<<<<<<< HEAD
-					<div class="panel-default pestana" id="tab3">
-=======
+
 					<div class="panel-default pestana" id="tab3{{$personal->id}}">
->>>>>>> 3959c603c50cefa9e10ac7950f3b15e83ae0fa03
+
 						<div class="panel-heading">
 							Contactos:
 						</div>
@@ -301,13 +224,7 @@
 						</div>
 						@endif
 					</div>
-					
-								
-<<<<<<< HEAD
-					<div class="panel-default pestana" id="tab4">
-=======
 					<div class="panel-default pestana" id="tab4{{$personal->id}}">
->>>>>>> 3959c603c50cefa9e10ac7950f3b15e83ae0fa03
 				 	<div class="panel-heading">Datos Generales:</div>
 				 	@if (count($personal->datosGenerales) == 0)
 						<div class="panel-body">
@@ -340,16 +257,11 @@
 				 	</div>
 				 	@endif
 				</div>
-<<<<<<< HEAD
-  				</div></div>
-@endforeach
-
-=======
   				</div>
   			</div>
   		</div>
 	</div>	
 	</div>
 @endforeach
->>>>>>> 3959c603c50cefa9e10ac7950f3b15e83ae0fa03
+
 @endsection
--- resources/views/paciente/create.blade.php
+++ resources/views/paciente/create.blade.php
@@ -40,20 +40,113 @@
 							<option>Otro</option>
 						</select>
 					</div>
+					{{-- PESTAÑAS --}}
 					<div>
 						<ul class="nav nav-pills">
 							<li class="active"><a href="#tab1"  class="ui-tabs-anchor">Generales:</a></li>
 
-							<li role="presentation" class="disabled" disabled="disabled"><a class="ui-tabs-anchor" disabled="disabled">Laborales:</a></li>
+							<li role="presentation"><a class="ui-tabs-anchor">Historial Medico:</a></li>
 
-							<li role="presentation" class="disabled" disabled="disabled"><a class="ui-tabs-anchor" disabled="disabled">Estudios:</a></li>
+							<li role="presentation"><a class="ui-tabs-anchor">Ocular:</a></li>
 
-							<li role="presentation" class="disabled" disabled="disabled"><a class="ui-tabs-anchor" disabled="disabled">Emergencias:</a></li>
+							<li role="presentation"><a class="ui-tabs-anchor">Ortopedico:</a></li>
 
-							<li role="presentation" class="disabled" disabled="disabled"><a class="ui-tabs-anchor" disabled="disabled">Vacaciones:</a></li>
+							<li role="presentation"><a class="ui-tabs-anchor">Citas:</a></li>
 
-							<li role="presentation" class="disabled" disabled="disabled"><a class="ui-tabs-anchor" disabled="disabled">Administrativo:</a></li>
+							<li role="presentation"><a class="ui-tabs-anchor">C.R.M.:</a></li>
 						</ul>
+					</div>
+					{{-- DATOS GENERALES --}}
+					<div class="panel-default">
+						<div class="panel-heading"><h5>Datos Generales:</h5></div>
+						<div class="panel-body">
+							<div class="col-xs-offset-2 form-group col-xs-4">
+								<label class="control-label">Ocupación:</label>
+								<input class="form-control" type="text">
+							</div>
+							<div class="form-group col-xs-4">
+								<label class="control-label">Convenio:</label>
+								<select class="form-control">
+									<option>Convenio 1</option>
+									<option>Convenio 2</option>
+									<option>Convenio ...</option>
+								</select>
+							</div>
+						</div>
+						<div class="panel-heading"><h6>Dirección:</h6></div>
+						<div class="panel-body">
+						<div class="form-group col-xs-3">
+							<label class="control-label">Calle:</label>
+							<input class="form-control" type="text">
+						</div>
+						<div class="form-group col-xs-3">
+							<label class="control-label">Número Interior:</label>
+							<input class="form-control" type="text">
+						</div>
+						<div class="form-group col-xs-3">
+							<label class="control-label">Número Exterior:</label>
+							<input class="form-control" type="text">
+						</div>
+						<div class="form-group col-xs-3">
+							<label class="control-label">Codigo Postal:</label>
+							<input class="form-control" type="text">
+						</div>
+						<div class="form-group col-xs-3">
+							<label class="control-label">Delegación/Municipio:</label>
+							<input class="form-control" type="text">
+						</div>
+						<div class="form-group col-xs-3">
+							<label class="control-label">Estado:</label>
+							<input class="form-control" type="text">
+						</div>
+						</div>
+						<div class="panel-heading"><h6>Contacto:</h6></div>
+						<div class="panel-body">
+						<div class="form-group col-xs-4">
+							<label class="control-label">Telefono Casa:</label>
+							<input class="form-control" type="text">
+						</div>
+						<div class="form-group col-xs-4">
+							<label class="control-label">Telefono Oficina:</label>
+							<input class="form-control" type="text">
+						</div>
+						<div class="form-group col-xs-4">
+							<label class="control-label">Telefono celular:</label>
+							<input class="form-control" type="text">
+						</div>
+						<div class="form-group col-xs-4">
+							<label class="control-label">Email:</label>
+							<input class="form-control" type="mail">
+						</div>
+						<div class="col-md-12 offset-md-2 mt-3">
+							<button class="btn btn-success">Guardar</button>
+	  						<p><strong>*Campo requerido</strong></p>
+						</div>
+						</div>
+						<div class="panel-heading">Tutores:</div>
+						<div class="panel-body">
+							<button class="btn btn-info">Agregar Tutores</button>
+							<div class="container">
+								
+								<table class="table table-striped table-bordered table-hover" style="color:rgb(51,51,51); border-collapse: collapse; margin-bottom: 0px">
+									<thead>
+										<tr class="info">
+											<th>Nombre</th>
+											<th>Apellido Paterno</th>
+											<th>Apellido Materno</th>
+											<th>Relación</th>
+											<th>Telefono Casa</th>
+											<th>Celular</th>
+											<th>Ver/Modificar</th>
+										</tr>
+									</thead>
+									
+										<tr class="active"{{--  onclick="vistarapida({{$personal->id}})" --}}>
+										</tr>
+									</tbody>
+								</table>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>

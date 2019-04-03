368d0113801df57435a6822ac0645625f1a07dbd
Daniel <dani_9_valerdi@hotmail.com>
2019-04-03 12:37:08 -0600

    Se quito inputs que no hacian nada y se añadio script para bootstrap, para usar modales que en los CRM no se mostraban

--- resources/views/layouts/blank.blade.php
+++ resources/views/layouts/blank.blade.php
@@ -21,6 +21,8 @@
         <link rel="stylesheet" href="{{ asset('css/style.css') }}">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+        <!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     </head>
     <body>
         <div id="app">
